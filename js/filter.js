5784a5e6727789f8681a62ee198efb713af5ff87
Zkikiki <140233651+Zkikiki@users.noreply.github.com>
2025-03-28 20:07:51 +0800

    update projects and timeline
    fixed bugs as projects' selection, add new projects

--- js/filter.js
+++ js/filter.js
@@ -1,49 +1,51 @@
+let rellax;
+document.addEventListener('DOMContentLoaded', function () {
+    if (document.querySelector('.rellax')) {
+        rellax = new Rellax('.rellax');
+    }
 
-filterSelection("all") // Execute the function and show all columns
+
+    const btnContainer = document.getElementById("filterBtn");
+    if (btnContainer) {
+        const btns = btnContainer.getElementsByClassName("filter-btn");
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                const current = document.querySelector(".filter-active");
+                if (current) {
+                    current.classList.remove("filter-active");
+                }
+                this.classList.add("filter-active");
+            });
+        }
+    }
+
+    // 初始显示所有
+    filterSelection("all");
+});
+
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filter-column");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements,
-  //and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "filter-show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "filter-show");
-  }
+    const columns = document.querySelectorAll(".filter-column");
+    const showAll = c === "all";
+
+    columns.forEach(col => {
+        const shouldShow = showAll || col.classList.contains(c);
+        col.classList.toggle("filter-show", shouldShow);
+    });
+
+    try {
+        if (rellax && typeof rellax.refresh === 'function') {
+            setTimeout(() => rellax.refresh(), 50); // 添加微小延迟确保DOM更新
+        }
+    } catch (e) {
+        console.warn("Rellax refresh error:", e);
+    }
 }
 
-// Show filtered elements
+
 function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
+    element.classList.add(...name.split(" "));
 }
 
-// Hide elements that are not selected
 function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("filterBtn");
-var btns = btnContainer.getElementsByClassName("filter-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("filter-active");
-    current[0].className = current[0].className.replace(" filter-active", "");
-    this.className += " filter-active";
-  });
+    element.classList.remove(...name.split(" "));
 }
